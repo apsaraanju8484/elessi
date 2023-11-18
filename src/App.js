18fb19d2d52f0fc1cee34e39dc4fa33f27595815
apsaraanju8484 <apsaramr1@gmail.com>
2023-11-18 19:35:57 +0530

    Inital commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,53 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Banner from './components/Banner';
+import Gallery from './components/Gallery';
+import Latest from './components/Latest';
+import Navbar from './components/Navbar/Navbar';
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import ShopCategory from './components/Pages/ShopCategory';
+import Product from './components/Pages/Product';
+import Menu from './components/Menu';
+import ProductDisplay from './components/ProductDisplay/ProductDisplay';
+import Instagram from './components/Instagram/Instagram';
+import Footer from './components/Footer/Footer';
+import Scrolling from './components/Scrolling/Scrolling';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+
+      <BrowserRouter>
+        <Menu />
+        <Banner />
+        <Gallery />
+
+        <Navbar />
+        <Routes>
+
+          <Route path='/all' element={<ShopCategory category="all" />} />
+          <Route path='/womens' element={<ShopCategory category="women" />} />
+          <Route path='/onsale' element={<ShopCategory category="onsale" />} />
+
+          <Route path='/mens' element={<ShopCategory category="men" />} />
+
+          <Route path='/new' element={<ShopCategory category="new" />} />
+
+         
+
+
+          <Route path='/product' element={<Product />} >
+            <Route path=':productId' element={<Product />} />
+          </Route>
+
+        </Routes>
+        <Scrolling/>
+        <Latest />
+        <Instagram/>
+        <Footer/>
+      </BrowserRouter>
+
+    </>
   );
 }
 
